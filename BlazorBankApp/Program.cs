e7991c279b659b3603f3996b7e696456014a24ab
DionMcD <doonsa11@hotmail.com>
2024-08-07 16:48:52 +1000

    Update Program.cs
    Added Comment

--- BlazorBankApp/Program.cs
+++ BlazorBankApp/Program.cs
@@ -6,7 +6,7 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using BlazorBankApp.Services;
-
+//The Program class defines the application's starting point.
 public class Program
 {
     public static async Task Main(string[] args)
