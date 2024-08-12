aeec11868dc9230c18b38ff005b11d38404812e3
DionMcD <doonsa11@hotmail.com>
2024-08-12 10:56:17 +1000

    Update BankService.cs

--- BlazorBankApp/Services/BankService.cs
+++ BlazorBankApp/Services/BankService.cs
@@ -1,16 +1,18 @@
 using BlazorBankApp.Models;
 
 namespace BlazorBankApp.Services;
-
+// This service class manages bank accounts for the application.
 public class BankAccountService
 {
-    //TODO add comments
+    //Property to store the everyday bank account.
     public BankAccount EverydayAccount { get; set; }
+    //Property to store the savings bank account.
     public BankAccount SavingsAccount { get; set; }
-
+    //Constructor to initialise the bank accounts.
     public BankAccountService(BankAccount everydayAccount, BankAccount savingsAccount)
-    {
+    {   //Set the everyday account.
         EverydayAccount = everydayAccount;
+        //Set the savings account.
         SavingsAccount = savingsAccount;
     }
 }
