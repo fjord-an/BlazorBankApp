5db24f5b0051e9b4e637a0091f3d95494a394e8a
DionMcD <doonsa11@hotmail.com>
2024-08-12 13:38:52 +1000

    Update Program.cs

--- BlazorBankApp/Program.cs
+++ BlazorBankApp/Program.cs
@@ -8,14 +8,23 @@ using System.Threading.Tasks;
 using BlazorBankApp.Models;
 using BlazorBankApp.Services;
 //The Program class defines the application's starting point.
+//In a Blazor WebAssembly application, the entry point is typically the Main method in the the Program class.
+//This is where the application is configured and launched.
 public class Program
 {
+    //The main method is the entry point for the Blazor WebAssembly application.
+    //It sets up and configures the application before running it asynchronously.
     public static async Task Main(string[] args)
     {
+        //Create a WebAssemblyHostBuilder instance using default settings.
+        //This sets up the host environment for the Blazor WebAssembly application.
+        //The HeadOutlet component is rendered in the "head::after" element.
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
+        //configure the HttpClient service to use a base address from the host environment.
+        //This HttpClient will be used to make HTTP requests to the server.
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
         // TODO elaborate on the scopes;
